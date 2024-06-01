e0e0527d6c8fb6b408ce400b2a68db65805d3154
Ahmadl-Al-Baghdadi <ahmadalbaghdadi2001@gmail.com>
2024-06-01 11:29:13 +0300

    update remove punctuatuion processor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from text_processors.word_tokenize_processor import TokenizationProcessor
+from text_processors.word_tokenize_processor import WordTokenizeProcessor
 from text_processors.remove_stopwords_processor import RemoveStopwordsProcessor
-from text_processors.stemmer_processor import StemmingProcessor
-from text_processors.lemmatizer_processor import LemmatizationProcessor
+from text_processors.stemmer_processor import StemmerProcessor
+from text_processors.lemmatizer_processor import LemmatizerProcessor
 from text_processors.spell_checker_processor import SpellCheckerProcessor
 from text_processors.remove_punctuation_processor import RemovePunctuationProcessor
 
@@ -12,3 +12,13 @@ from text_processors.remove_punctuation_processor import RemovePunctuationProces
 # spellChecker = SpellCheckerProcessor(stopWords.tokens)
 removePunc = RemovePunctuationProcessor("'Hello, world! This is some sample' text.''")
 print(removePunc.get_tokens())
+
+# def load_csv(filepath):
+#     data = pd.read_csv(filepath, usecols=['id_right', 'text_right'], nrows=40000)
+#     data['processed'] = data['text_right'].apply(
+#         preprocess_text)  # Replace 'text_column' with the actual text column name
+#     return data
+
+# csv_data = load_csv('C:\Users\Ahmad\Desktop\ir\dataset\wikIR1k\documents.csv')
+# print('ahmad')
+# print(csv_data)
